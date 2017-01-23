d513f81ad5e707ec9a3fc1de9915579a982c0ca3
Evgeny Myltsev <myltsev.ea@gmail.com>
2017-01-23 12:42:20 +0300

    'Rename'

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples component!</h3>
+      <h3>Examples</h3>
       <p>Wellcome to exapmles page</p>
     </div>
     );
